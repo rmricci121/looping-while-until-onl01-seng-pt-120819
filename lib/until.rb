34f6d7458eac6761bcfc40e74966446e71fbdbc2
Robert M Ricci <rmricci121@gmail.com>
2019-12-11 00:58:10 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,6 @@
 def using_until
   levitation_force = 6
-   levitation_force = 6
-	while levitation_force == 10 do
+	until levitation_force == 10 do
 	  puts "Wingardium Leviosa"
 	  levitation_force += 1
 	end
